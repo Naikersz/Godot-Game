ba40ac4aae344bf2cd084d2a60371ace1434f756
Naikersz <rusik16072001@gmail.com>
2025-11-28 12:03:44 +0100

    Godot 1.0

---
+++ Skripts/level_town.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+## Stub-Script für die LevelTown-Szene.
+## Hier kann später Logik für UI, Pausenmenü usw. ergänzt werden.
+
+func _ready() -> void:
+	# Aktuell keine zusätzliche Logik nötig.
+	pass
